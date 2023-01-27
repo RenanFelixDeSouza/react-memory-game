2569f3b1863a6d5787db0ac158f44adc272ed416
Renan Felix <renanrenan050900@gmail.com>
2023-01-26 22:32:25 -0400

    executavel

---
+++ src/templates/App/index.tsx
@@ -0,0 +1,25 @@
+import Card from '../../components/Card'
+import { Grid } from '../../components/Grid'
+import { cards } from '../../data/cards'
+import './styles.css'
+
+const handleClick = (id: string) => {
+  console.log(id)
+}
+
+
+function App() {
+
+  return (
+ 
+
+
+  <div className="app">
+ <Grid cards={cards}/> 
+  </div>
+
+
+  )
+}
+
+export default App
---
+++ src/utils/card-utils.ts
@@ -0,0 +1,31 @@
+import Card, { CardProps } from '../components/Card';
+import { cards } from '../data/cards';
+
+const keyGen = (): string => {
+  return (
+    Math.random().toString(36).substring(2,15) +
+    Math.random().toString(36).substring(2,15)
+  );
+}
+
+export const duplicateArray =  <T>(array: T[]): T[] => {
+   return array.concat(array);
+};
+
+export const sortArray = <T>(array: T[]): T[] => {
+  return array.sort(() => Math.random() - 0.5);
+};
+
+
+
+export const regenerateCard = (cards: CardProps[]): CardProps[] => {
+  return cards.map((card) => ({...card, id: keyGen()}))
+}
+
+export const duplicateRegenerateSortArray = (card: CardProps[]): CardProps[] => {
+  return sortArray(regenerateCard(duplicateArray(cards)))
+};
+
+
+
+
